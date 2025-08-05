8bb4c8cea63cc9a16ffe3ce458e21735eb05389d
Georgina Da Luz <daluz.georgina@gmail.com>
2025-08-05 12:29:20 -0300

    chore: add Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // ← ¡todas tus rutas de React!
+  ],
   theme: {
     extend: {
       colors: {
